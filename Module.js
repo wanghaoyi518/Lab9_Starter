7c885c3a9b92ddfa32d9b4231061d36eea60a632
Haoyi <haw082@ucsd.edu>
2022-11-27 21:58:57 -0800

    updateScreenShot

---
+++ Module.js
@@ -0,0 +1,8 @@
+// ES6 Modular JavaScript. 
+// npm install trackjs --save
+
+import { TrackJS } from 'trackjs';
+TrackJS.install({
+  token: "4210f93ec2754aefacd9b9ae55100811"
+  // for more configuration options, see https://docs.trackjs.com
+});
